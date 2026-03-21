6ecf02e3615d6a7fa27126f3a366e76fd5f4767a
Kabelo Hlako <kabelohlako.kh@gmail.com>
2026-03-21 02:28:43 +0200

    front end setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react"
+import Navbar from './components/Navbar.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Navbar />
 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='login' element={<LoginPage/>}/>
+        <Route path='/settings' element={<SettingsPage/>}/>
+        <Route path='profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
-    )
+  )
 }
 
 export default App
